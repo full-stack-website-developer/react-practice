3fd4cc35638cbabcc41f3329806f80d101949f89
full-stack-website-developer <talhasajjad148@gmail.com>
2025-08-12 23:00:35 +0500

    vite-error-fixed

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['@fortawesome/react-fontawesome']
-    }
-  },
   optimizeDeps: {
     include: ['@fortawesome/react-fontawesome']
   }
